5fc3ea7872b39a901dd9264b5215282565393ca7
vinter <vinter87@ymail.com>
2025-04-02 16:56:37 +0800

    Update groups.ts

--- src/Socket/groups.ts
+++ src/Socket/groups.ts
@@ -357,7 +357,7 @@ export const extractGroupMetadata = (result: BinaryNode) => {
 		participants: getBinaryNodeChildren(group, 'participant').map(
 			({ attrs }) => {
 				return {
-					id: attrs.jid,
+					id: attrs.phone_number || attrs.jid,
 					admin: (attrs.type || null) as GroupParticipant['admin'],
 				}
 			}
